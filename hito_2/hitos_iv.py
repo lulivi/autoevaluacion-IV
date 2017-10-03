85eecb717b49316c5d2e1b55f8573cd463a17d0b
lulivi <luislivilla@gmail.com>
2017-10-03 17:31:00 +0200

    Añadida lectura de JSON closes #17

--- hito_2/hitos_iv.py
+++ hito_2/hitos_iv.py
@@ -1,8 +1,23 @@
+#!/usr/bin/env python
 """Modulo de funciones para manejar el archivo JSON de los hitos."""
 
 import json
 
 
-def json_reader(json_file):
-    """Load JSON file into a dictionary."""
-    pass
+def read_json(json_file='hitos.json'):
+    """Devuelve un diccionario con el archivo JSON."""
+    milestone_dict = {}
+    try:
+        with open(json_file, 'r') as f:
+            milestone_dict = json.loads(f.read())
+    except Exception as e:
+        raise
+    finally:
+        return milestone_dict
+
+
+if __name__ == '__main__':
+    milestones = read_json()
+    print(
+        json.dumps(
+            milestones, sort_keys=True, indent=2, separators=(',', ' : ')))
