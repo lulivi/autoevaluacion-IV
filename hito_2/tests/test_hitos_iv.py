fed8fd663e6e85f8d37c26b623ffb483e5460c9b
lulivi <luislivilla@gmail.com>
2017-10-03 19:56:40 +0200

    Añadido test para obtención de milestone

--- hito_2/tests/test_hitos_iv.py
+++ hito_2/tests/test_hitos_iv.py
@@ -12,14 +12,24 @@ class HitosIvTest(unittest.TestCase):
     def test_read_json(self):
         """Comprueba si lee correctamente el fichero."""
         self.assertFalse(
-            hitos_iv.read_json('../hitos.json') == {},
-            'Diccionario no vacío')
+            hitos_iv.read_json('../hitos.json') == {}, 'Diccionario no vacío')
 
     def test_milestone_number(self):
-        """Test para obtenerr numero de hitos."""
+        """Comprueba si el numero de milestones es correcto."""
         mil_dict = hitos_iv.read_json('../hitos.json')
         self.assertEqual(
-            hitos_iv.milestone_number(mil_dict), 2, "Número correcto de hitos")
+            hitos_iv.milestone_number(mil_dict), 2, 'Número correcto de hitos')
+
+    def test_get_milestone(self):
+        """Comprueba si devuelve un objeto milestone correcto."""
+        mil_dict = hitos_iv.read_json('../hitos.json')
+        self.assertFalse(
+            hitos_iv.get_milestone(mil_dict, 0) == {}, 'Hito no vacío')
+        self.assertFalse(
+            hitos_iv.get_milestone(mil_dict, 1) == {},
+            'Accede a indice correcto')
+        with self.assertRaises(IndexError):
+            hitos_iv.get_milestone(mil_dict, 2)
 
 
 if __name__ == '__main__':
