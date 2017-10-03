dd4ac32480a488e366d28698c0d77f326d41b893
lulivi <luislivilla@gmail.com>
2017-10-03 17:29:47 +0200

    Añadido test para lectura de JSON closes #16

--- hito_2/tests/test_hitos_iv.py
+++ hito_2/tests/test_hitos_iv.py
@@ -1,18 +1,19 @@
-#!/bib/bash
+#!/usr/bin/env python
 """Modulo de test para el archivo hitos_iv.py."""
 
 import unittest
-# import sys
+import __init__
 import hitos_iv
 
 
 class HitosIvTest(unittest.TestCase):
     """Clase para testing del modulo hitos_iv."""
 
-    def test_milestone_number(self):
-        """Test para obtenerr numero de hitos."""
-        self.assertEqual(hitos_iv.milestone_number(), 2,
-                         "Número correcto de hitos")
+    def test_read_json(self):
+        """Comprueba si lee correctamente el fichero."""
+        self.assertFalse(
+            hitos_iv.read_json('../hitos.json') == {},
+            'Diccionario no vacío')
 
 
 if __name__ == '__main__':
