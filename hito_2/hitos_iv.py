00ead6a5d94ba658af90753a7f2fdee6eceb41e9
lulivi <luislivilla@gmail.com>
2017-10-03 19:57:38 +0200

    Añadida función que devuelve un hito]

--- hito_2/hitos_iv.py
+++ hito_2/hitos_iv.py
@@ -2,18 +2,21 @@
 """Modulo de funciones para manejar el archivo JSON de los hitos."""
 
 import json
+import logging
+
+
+logging.basicConfig(filename='tests.log', level=logging.DEBUG)
 
 
 def read_json(json_file='hitos.json'):
     """Devuelve un diccionario con el archivo JSON."""
-    milestone_dict = {}
     try:
+        milestone_dict = {}
         with open(json_file, 'r') as f:
             milestone_dict = json.loads(f.read())
     except Exception as e:
         raise
-    finally:
-        return milestone_dict
+    return milestone_dict
 
 
 def milestone_number(milestone_dict):
@@ -21,9 +24,27 @@ def milestone_number(milestone_dict):
     return len(milestone_dict['hitos'])
 
 
+def get_milestone(milestone_dict, milestone_id):
+    """Devuelve un hito en concreto."""
+    try:
+        milestone = {}
+        milestone = milestone_dict['hitos'][milestone_id]
+    except Exception as e:
+        raise
+    return milestone
+
+
 if __name__ == '__main__':
     milestones = read_json()
+    print('funcion read_json')
     print(
         json.dumps(
-            milestones, sort_keys=True, indent=2, separators=(',', ' : ')))
-    print('Milestone number: ', milestone_number(milestones))
+            milestones, indent=2, separators=(',', ' : ')))
+    print('funcion milestone_number')
+    print(milestone_number(milestones))
+    print('funcion get_milestone')
+    print(
+        json.dumps(
+            get_milestone(milestones, 2),
+            indent=2,
+            separators=(',', ' : ')))
